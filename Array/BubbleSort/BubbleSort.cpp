fcc40ef2225440397e5938448ffa8227c823324a
Himanshu Vishwakarma <himanshuvishwakarma383@gmail.com>
2025-09-20 20:59:20 +0530

    added initial cmmit

---
+++ Array/BubbleSort/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int arr[],int n){
+for(int i=0;i<n-1;i++){
+    bool isSwap=false;
+    for(int j=0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+  if(!isSwap){
+    return ;
+  }
+}
+
+}
+
+void printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout <<arr[i] << " ";
+
+    }
+    cout << endl;
+
+}
+
+
+int main(){
+    int n=5;
+    int arr[]={4,1,5,2,3};
+    BubbleSort(arr,n);
+    printArr(arr,n);
+    return 0;
+}
