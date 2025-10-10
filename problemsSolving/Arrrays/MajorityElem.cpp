5c739c07574937d0eabade411e543ba24d466cb3
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-10-10 14:51:27 +0530

    working on majorityelem  problem

--- problemsSolving/Arrrays/MajorityElem.cpp
+++ problemsSolving/Arrrays/MajorityElem.cpp
@@ -1,15 +1,50 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
+
 using namespace std;
 
-void MajorityElem(int arr[]){
-int size=sizeof(arr);
+void MajorityElem(vector<int>& arr){  
+   int size=arr.size();
+    for(int val : arr){  
+      int freq=0;
+
+          for(int el:arr){
+            if(val==el){
+               freq=freq+1;
+            }
+          }
+          if(freq >size/2){
+            cout << "The majority Elem is: "<< val ;
+          }
+    }
+}
+void MajorityElemSort(vector<int>& arr){
+   int size=arr.size();
+   int frequency=0;
+   std::sort(arr.begin(),arr.end());
+   for(int i=0;i<size;i++){
+      if(arr[i]==arr[0]){
+         frequency=frequency+1;
+
+      }
+      else{
+         frequency=0;
+      }
+
+   }
+   cout <<"The frequency of elem is:";
+
+   
 
 }
 
+int main(){
 
- int main(){
-    int arr[]={1,2,2,1,1};
-    MajorityElem(arr);
+   // using BruteForce Algo Technique
+    vector<int> arr = {1,2,2,1,1};   
+    MajorityElem(arr);  
+   // using InBuilt Sorting Technique
+   MajorityElemSort(arr);         
     return 0;
- }
+}
