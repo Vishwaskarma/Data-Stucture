5609f8c36f8d86f079a987d17f953813f489a3d5
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-10-24 12:54:07 +0530

    Implemented Kadane Algorithm

--- Array/Algorithms_Technique/Kadane'sAlgo/KadaneAlgo.cpp
+++ Array/Algorithms_Technique/Kadane'sAlgo/KadaneAlgo.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 
@@ -14,11 +15,28 @@ void maximumSubArray(int arr[],int n){
     }
 
 }
+//Implementation of kadane algorithm
+int maxiMumSubarraySum(vector<int>arr){
+    int currentMax=0;
+    int globalMax=0;
+    for(int i=0;i<arr.size();i++){
+        currentMax=max(arr[i],currentMax + arr[i]);
+        if(currentMax > globalMax){
+            globalMax=currentMax;
+        }
+    }
+    return globalMax;
+
+}
 
 
 int main(){
     int n=5;
     int arr[]={1,2,3,4,5};
     maximumSubArray(arr,n);
+    vector<int>arr2 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int ans=maxiMumSubarraySum(arr2);
+    cout << "The Maximum Subarray Sum is : " << ans << endl;
+
     return 0;
 }
