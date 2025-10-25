1da6b1d7f74b33a32b26b6fcd167c95e94cfe3ba
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-10-25 13:31:34 +0530

    Pulling my office Leraning algorithms

--- problemsSolving/Arrrays/PrimeNumAlgo/PrimeNumber.cpp
+++ problemsSolving/Arrrays/PrimeNumAlgo/PrimeNumber.cpp
@@ -41,12 +41,30 @@ void FindPrimeWithoutRange(int num){
 
 }
 
+int SieveOfEratosThenes(int n=50){
+    vector<bool>isPrime(n+1,true);
+    int count=0;
+    for(int i=2;i<n;i++){
+        if(isPrime[i]){
+            count++;
+            for(int j=i*2;j<n;j=j+=i){
+                isPrime[j]=false;
+            }
+
+        }
+    }
+    return count;
+
+}
 int main(){
     int start=2;
     int end=100;
     FindRangeOfPrime(start,end);
     int num=13;
     FindPrimeWithoutRange(num);
+
+    int ans=SieveOfEratosThenes();
+    cout <<"The total number of prime numbe is : " << ans << endl;
      return 0;
 
 }
