e85215acb6b51948d811f01b911c2bcca570e643
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-11-03 21:37:51 +0530

    Working on DigitsInNumber

---
+++ MathsForDatastructure/DigitsInNumber/DigitsInNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ void printDigits(int Digits){
+    
+
+ }
+int main(){
+    int digits=34532;
+
+    printDigits(digits);
+    return 0;
+
+}
