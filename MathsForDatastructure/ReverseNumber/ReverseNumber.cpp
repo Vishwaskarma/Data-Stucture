20028116c5a9265b5cd3c53b611c19e068129524
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-11-09 19:26:19 +0530

    Implemented to check number is pallindrome or not

--- MathsForDatastructure/ReverseNumber/ReverseNumber.cpp
+++ MathsForDatastructure/ReverseNumber/ReverseNumber.cpp
@@ -1,18 +1,27 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<climits>
 using namespace std;
 
 int ReverseNumber(int num){
     int ReversedNum=0;
     while(num!=0){
         int digit=num %10;
+        if(ReversedNum > INT_MAX || ReversedNum < INT_MIN)return 0;
         ReversedNum=ReversedNum*10 + digit;
         num=num/10;
 
     }
     return ReversedNum;
 
+}
+
+//check palindrome in number
+bool isPallindrome(int n){
+    if(n<0)return false;
+    int reverseNum=ReverseNumber(n);
+    return n==reverseNum;
 }
 
 
