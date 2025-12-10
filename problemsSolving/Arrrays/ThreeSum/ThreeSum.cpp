a3056a9d7832a1df4a57aa1fd2e5e0e281f4a03d
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-12-10 17:25:17 +0530

    Implementing optimized approach of three sum

--- problemsSolving/Arrrays/ThreeSum/ThreeSum.cpp
+++ problemsSolving/Arrrays/ThreeSum/ThreeSum.cpp
@@ -1,22 +1,56 @@
 #include<iostream>
 #include<vector>
+#include<set>
+#include<algorithm>
 using namespace std;
-vector<vector<int>> ThreeSum(vector<int>& arr) {
-    vector<vector<int>> ans;
 
-    for(int i = 0; i < arr.size(); i++) {
-        for(int j = i + 1; j < arr.size(); j++) {
-            for(int k = j + 1; k < arr.size(); k++) {
 
-                if(arr[i] + arr[j] + arr[k] == 0) {
-                    ans.push_back({arr[i], arr[j], arr[k]});
+// brute force approach
+vector<vector<int>>ThreeSumBrute(vector<int>& arr) {
+    set<vector<int>>ans;
+    for(int i=0;i<arr.size();i++){
+        for(int j=i+1;j<arr.size();j++){
+            for(int k=j+1;k<arr.size();k++){
+                if(arr[i]+arr[j]+arr[k]==0){
+                   vector<int>temp={arr[i],arr[j],arr[k]};
+                   sort(temp.begin(),temp.end());
+
+                   if(ans.find(temp)!=ans.end()){
+                    ans.insert(temp);
+                   }
+                    
                 }
+
             }
         }
     }
-
-    return ans;
+ return vector<vector<int>>(ans.begin(), ans.end());
 }
+
+
+
+//this is optimized approach but not he best
+vector<vector<int>> ThreeSum(vector<int>& arr) {
+    set<vector<int>> ans;
+
+    for (int i = 0; i < arr.size(); i++) {
+        set<int> hash;
+        for (int j = i + 1; j < arr.size(); j++) {
+            
+            int third = -(arr[i] + arr[j]);
+            if (hash.count(third)) {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                ans.insert(temp);
+            }
+
+            hash.insert(arr[j]);
+        }
+    }
+
+    return vector<vector<int>>(ans.begin(), ans.end());
+}
+
 
 
 int main() {
@@ -27,6 +61,13 @@ int main() {
     for(auto &triplet : ans) {
         for(int x : triplet) {
             cout << x << " ";
+        }
+        cout << endl;
+    }
+     vector<vector<int>> ansBrute = ThreeSumBrute(arr);
+     for(auto &triplet : ansBrute) {
+        for(int x : triplet) {
+            cout <<"This is BruteForce approach"<<  x << " ";
         }
         cout << endl;
     }
