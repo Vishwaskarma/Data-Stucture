3950356ddb7a8d27ca6059065e353a7d831fa337
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-12-17 16:32:28 +0530

    Implemented Four sum

---
+++ problemsSolving/Arrrays/SubArraySum/SubArraySum.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int SubArraySumBrute(vector<int>& arr,int k){
+int count=0;
+for(int i=0;i<arr.size()-1;i++){
+    int sum=0;
+    for(int j=0;j<arr.size()-1;j++){
+        sum+=arr[j];
+        if(sum==k){
+            count++;
+        }
+    }
+}
+return count;
+
+}
+
+
+
+
+
+
+
+
+int  main() {
+    vector<int> arr = {1,2,3};
+    int k=3;
+
+   int ans=SubArraySumBrute(arr,k);
+   cout << ans << " ";
+   return 0;
+}
