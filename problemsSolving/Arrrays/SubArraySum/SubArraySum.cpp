889e3525b3171eff740816a28f60f773d18a8da0
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-12-22 16:54:26 +0530

    Implemented SubArraySum using unorderd map and prefixsum technique

--- problemsSolving/Arrrays/SubArraySum/SubArraySum.cpp
+++ problemsSolving/Arrrays/SubArraySum/SubArraySum.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 using namespace std;
 
 
@@ -11,16 +12,36 @@ for(int i=0;i<arr.size()-1;i++){
     for(int j=0;j<arr.size()-1;j++){
         sum+=arr[j];
         if(sum==k){
-            count++;
+            count++; 
         }
     }
 }
 return count;
 
 }
+int subArraySumEqualToK(vector<int>& arr,int k){
+
+    int count=0;
+
+    unordered_map<int,int>mpp;
+    mpp[0]=1;
+    int prefixSum=0;
+    
+    for(int j=0;j<arr.size();j++){
+        prefixSum+=arr[j];
+        int remove=prefixSum-k;
+        count+=mpp[remove];
+        mpp[prefixSum]+=1;
+        
+    }
 
 
+    return count;
 
+}
+
+
+  
 
 
 
@@ -31,6 +52,8 @@ int  main() {
     int k=3;
 
    int ans=SubArraySumBrute(arr,k);
-   cout << ans << " ";
+   cout <<"This is BruteForce Approach is:"<< ans << endl;
+   int ans2=subArraySumEqualToK(arr,k);
+   cout <<"This is optimized way is:"<< ans2 << endl;
    return 0;
 }
