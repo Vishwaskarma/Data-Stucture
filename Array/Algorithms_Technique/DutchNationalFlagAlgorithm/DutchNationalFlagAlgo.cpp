0a0fac95eaabd7b1351fd5733843a09ebd523533
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-12-30 12:52:24 +0530

    Implemented Dutch national flag Algorithm

--- Array/Algorithms_Technique/DutchNationalFlagAlgorithm/DutchNationalFlagAlgo.cpp
+++ Array/Algorithms_Technique/DutchNationalFlagAlgorithm/DutchNationalFlagAlgo.cpp
@@ -1,10 +1,40 @@
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
 using namespace std;
 
+vector<int> DutchNationalFlagAlgo(vector<int> &arr)
+{
 
+    int mid = 0;
+    int low = 0;
+    int high = arr.size() - 1;
 
-int main(){
+    while (mid <= high)
+    {
+        if(arr[mid]==0){
+            swap(arr[mid],arr[low]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+
+        }else{
+            swap(arr[mid],arr[high]);high--;
+        }
+    }
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {2, 0, 2, 0, 1, 1, 0, 1, 2};
+    vector<int> ans = DutchNationalFlagAlgo(arr);
     
+    for(int x :ans){
+
+        cout <<"This is DutchNationalFlagAlgo: " <<  x<< endl;
+    } ;
+
     return 0;
 }
