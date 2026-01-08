cbda095fa3c1328e50478644d44b4ee61731c576
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2026-01-08 15:01:17 +0530

    Working on closest sum of target Elem and valid parenthersis

---
+++ Strings/ValidParenthesis/ValidParenthesis.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void ValidParenthesis(string s){
+  
+    
+
+}
+
+
+int main(){
+    string s="()[]{}";
+    ValidParenthesis(s);
+    
+   
+    return 0;
+}
---
+++ problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+
+int threeSumClosest(vector<int>&arr,int target){
+    int sum;
+    for(int i=0;i<arr.size()-1;i++){
+        
+
+        }
+
+    }
+
+
+
+
+int main(){
+    vector<int>arr={-1,2,1,-4};
+    int target=1;
+    int closest=threeSumClosest(arr,target);
+    return 0;
+
+}
