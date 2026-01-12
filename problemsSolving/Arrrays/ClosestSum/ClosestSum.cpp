7289542884921de3f24e9e3ec3e14d93f5c3c251
Your Name <you@example.com>
2026-01-12 20:50:27 +0530

    Implementing find closest sum of target elem

--- problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
+++ problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
@@ -1,24 +1,29 @@
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
 using namespace std;
 
-
-int threeSumClosest(vector<int>&arr,int target){
+int threeSumClosest(vector<int> &arr, int target)
+{
+    int finalSum=0;
     int sum;
-    for(int i=0;i<arr.size()-1;i++){
-        
 
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        sum = 0;
+
+        for (int j = i + 1; j < arr.size() - 1; j++)
+        {
+            sum += arr[i] + arr[j];
+
+            if(sum>target || sum<finalSum)
         }
-
     }
+}
 
-
-
-
-int main(){
-    vector<int>arr={-1,2,1,-4};
-    int target=1;
-    int closest=threeSumClosest(arr,target);
+int main()
+{
+    vector<int> arr = {-1, 2, 1, -4};
+    int target = 1;
+    int closest = threeSumClosest(arr, target);
     return 0;
-
 }
