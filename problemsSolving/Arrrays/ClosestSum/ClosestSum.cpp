c8d35cf8c4d616de81b75ca62ead08c5a27dcf90
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2026-01-19 17:14:54 +0530

    Implementing remove duplicates from an sorted array

--- problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
+++ problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
@@ -1,29 +1,51 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+// BruteForce Approach
 int threeSumClosest(vector<int> &arr, int target)
 {
-    int finalSum=0;
-    int sum;
-
-    for (int i = 0; i < arr.size() - 1; i++)
+    int n = arr.size() - 2;
+    int closestSum = arr[0] + arr[1] + arr[2];
+    int closestDifference = INT_MAX;
+    sort(arr.begin(),arr.end());
+    for (int i = 0; i < n; i++)
     {
-        sum = 0;
-
-        for (int j = i + 1; j < arr.size() - 1; j++)
+        int left = i + 1;
+        int right = arr.size() - 1;
+        while (left < right)
         {
-            sum += arr[i] + arr[j];
+            int sum = arr[i] + arr[left] + arr[right];
+            if (sum == target)
+            {
+                return target;
+            }
+            if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+            int differenceTarget = abs(sum - target);
+            if (differenceTarget<closestDifference ){
+                closestSum=sum;
+                closestDifference=differenceTarget;
 
-            if(sum>target || sum<finalSum)
+
+            }
         }
     }
+    return closestSum;
 }
 
 int main()
 {
-    vector<int> arr = {-1, 2, 1, -4};
+    vector<int> arr = {-1,2,1,-4};
     int target = 1;
-    int closest = threeSumClosest(arr, target);
+    int ans = threeSumClosest(arr, target);
+    cout << ans << endl;
     return 0;
 }
