470115e5ce6dc26bc102067e2a05f8850b84d8b1
Your Name <you@example.com>
2026-01-20 21:09:51 +0530

    My Initial commit

--- problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
+++ problemsSolving/Arrrays/ClosestSum/ClosestSum.cpp
@@ -4,20 +4,26 @@ using namespace std;
 
 int threeSumClosest(vector<int> &arr, int target)
 {
-    int finalSum=0;
-    int sum;
+    int AfterCheckPush=0;
+    int CurrentSum=0;
 
     for (int i = 0; i < arr.size() - 1; i++)
     {
-        sum = 0;
-
-        for (int j = i + 1; j < arr.size() - 1; j++)
-        {
-            sum += arr[i] + arr[j];
-
-            if(sum>target || sum<finalSum)
+        CurrentSum+=arr[i];
+        if(CurrentSum>target && CurrentSum<AfterCheckPush){
+            return CurrentSum;
+        }else if(CurrentSum<target && CurrentSum>AfterCheckPush){
+            return CurrentSum;
         }
+        else{
+            AfterCheckPush=CurrentSum;
+
+
+        }
+
+       
     }
+    return CurrentSum;
 }
 
 int main()
@@ -25,5 +31,6 @@ int main()
     vector<int> arr = {-1, 2, 1, -4};
     int target = 1;
     int closest = threeSumClosest(arr, target);
+    cout<<closest<< endl;
     return 0;
 }
