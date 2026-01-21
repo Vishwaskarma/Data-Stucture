fd07dab0e68d401de302b57c4a062e4853ee0db8
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2026-01-21 10:32:54 +0530

    Implemented Remove Duplicates from sorted array

--- problemsSolving/Arrrays/RemoveDuplicates/RemoveDuplicates.cpp
+++ problemsSolving/Arrrays/RemoveDuplicates/RemoveDuplicates.cpp
@@ -4,29 +4,18 @@
  using namespace std;
 
 
- pair<vector<int>,int>removeDuplicates(vector<int>& nums) {
-       
-      int count=0;
-      for(int i=0;i<nums.size()-1;i++){
-     
-        int end=nums.size()-1;
-
-        while(nums[i]<end){
-            if(nums[i]==nums[end]){
-                count+=1;
-                nums.erase(nums.begin()+1);
-                nums.push_back('_');
-                end--;
-            }
-        }
-      }
-       return nums,count;
+int removeDuplicates(vector<int>& nums) {
+       int j=1;
+       for(int i=1;i<nums.size();i++)
+       if(nums[i]!=nums[i-1]) {nums[j]=nums[i];j++;}
+       return j;
         
     }
  
  int main(){
     vector<int>nums= {1,1,2};
-    pair<vector<int>,int>ans= removeDuplicates(nums);
+    int ans= removeDuplicates(nums);
+    cout << ans <<endl;
     return 0;
  }
  
