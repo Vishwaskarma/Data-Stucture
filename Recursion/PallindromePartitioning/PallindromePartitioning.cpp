f393a242628874af02c08b57c186adb32e01d90b
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2026-01-28 13:39:28 +0530

    Implemented Pallindrome Partitioning completed

--- Recursion/PallindromePartitioning/PallindromePartitioning.cpp
+++ Recursion/PallindromePartitioning/PallindromePartitioning.cpp
@@ -3,35 +3,43 @@
 #include<string>
 using namespace std;
 
-bool checkPallindrome(string s,string subpart){
+
+
+bool checkPallindrome(string subpart){
     int st=0;
-    int end=subpart.size();
-    while(st<=end)if(s[st++]==s[end--]);
+    int end=subpart.size()-1;
+    while(st<=end) if(subpart[st++] != subpart[end--])  return false;
     return true;
 }
 
-vector<vector<string>>PallindromePartitioning(string s,vector<string>subpart,vector<vector<string>>ans){
-    if (s.size()==0)ans.push_back({subpart});return;
-    for(int i=0;i<s.size()-1;i++){
+void PallindromePartitioning(string s,vector<string>&subpart,vector<vector<string>>&ans){
+    if (s.size()==0) {
+    ans.push_back(subpart);
+    return;  
+    }
+    for(int i=0;i<s.size();i++){
     string sub=s.substr(0,i+1);
-    if(checkPallindrome(s,sub))subpart.push_back({sub});
-    PallindromePartitioning(s.substr(i+1),subpart,ans);
-    subpart.pop_back();
+   if(checkPallindrome(sub)) {
+   subpart.push_back(sub);  
+   PallindromePartitioning(s.substr(i+1),subpart,ans);
+   subpart.pop_back();
+        }
+
 }
-return ans;
+
 }
 
 int main(){
     string s="aab";
     vector<string>Partitions;
     vector<vector<string>> PartitionsAns;
-    vector<vector<string>>answer=PallindromePartitioning(s,Partitions,PartitionsAns);
-    for (const auto& row : answer) {
-    for (string value : row) {
-        std::cout << value << " ";
+    PallindromePartitioning(s,Partitions,PartitionsAns);
+    for (const auto& row : PartitionsAns) {
+        for (string value : row) {
+            cout << value << " ";
+        }
+        cout << endl;
     }
-    std::cout << std::endl;
-}
     return 0;
 
 }
