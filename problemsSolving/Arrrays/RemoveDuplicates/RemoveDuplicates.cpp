9c1f86d3b49609eb0f5adf10066827b457cfabcd
Your Name <you@example.com>
2026-02-05 21:32:24 +0530

    Implemented Remove Duplicates II from sorted array

--- problemsSolving/Arrrays/RemoveDuplicates/RemoveDuplicates.cpp
+++ problemsSolving/Arrrays/RemoveDuplicates/RemoveDuplicates.cpp
@@ -5,9 +5,10 @@
 
 
 int removeDuplicates(vector<int>& nums) {
+       if(nums.size() == 0) return 0;
        int j=1;
        for(int i=1;i<nums.size();i++)
-       if(nums[i]!=nums[i-1]) {nums[j]=nums[i];j++;}
+       if(nums[i]!=nums[i-1]) { j++;nums[j]=nums[i];}
        return j;
         
     }
--- problemsSolving/Arrrays/RemoveDuplicatesSortedArrSec/RemoveDuplicatesSortedArrSec.cpp
+++ problemsSolving/Arrrays/RemoveDuplicatesSortedArrSec/RemoveDuplicatesSortedArrSec.cpp
@@ -2,18 +2,26 @@
 #include<vector>
 using namespace std;
 
+
+
 int RemoveDuplicatesFromSortedArrarSecond(vector<int>&arr){
-    int count=0;
-    for(int i=0;i<arr.size();i++){
-        cout << arr[i]<<endl;
-    }
-    return count;
+    int left=1;
+        int count=1;
+        if(arr.size()<1)return 0;
+        if(left<arr.size())
+        for(int i=1;i<arr.size();i++){
+        arr[i]==arr[i-1]?count+=1:count=1;
+        if(count<=2)arr[left++]=arr[i];
+        }
+        
+        return left;
 
 }
 
 int main(){
     vector<int>nums = {1,1,1,2,2,3};
     int ans=RemoveDuplicatesFromSortedArrarSecond(nums);
-    cout<< ans <<endl;
+    cout<< ans<<endl;
+
     return 0;
 }
