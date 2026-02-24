3b35be8263419f4e2aab8c8e64484ec87ce08cf5
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2026-02-24 16:01:49 +0530

    Implemented Counting Inversion using optimal approach

--- Recursion/CountInversion/CountInversion.cpp
+++ Recursion/CountInversion/CountInversion.cpp
@@ -16,7 +16,7 @@ vector<vector<int>>findInversionPair(vector<int>&arr){
             }
         }
     }
-    return pairs;
+    return pairs;   
 }
 //printing count of inversion of pairs
 int inversionCount(vector<int>&arr){
@@ -30,8 +30,41 @@ int inversionCount(vector<int>&arr){
 }
 
 // Optimized approach
+    int Merge(vector<int>&arr,int start,int mid,int end){
+    vector<int>Temprory(end-start+1);
+    int left=start,right=mid+1,index=0;
+    int inversionCount=0;
+    while(left<=mid && right<=end){
+      if  (arr[left]<=arr[right] ){ Temprory[index++]=arr[left++];}
+     else{Temprory[index++]=arr[right++];inversionCount+=(mid-left+1);}
+    }
+    // condition if left array still have elem
+    while(left<=mid){Temprory[index++]=arr[left++];}
+    //condition if right array still have elem
+    while(right<=end){Temprory[index++]=arr[right++];}
+    for(int x:Temprory)cout <<"This is Temporary Array"<<  x << endl;
+
+    for(int i = 0; i < Temprory.size(); i++)
+    arr[start + i] = Temprory[i];
+     return inversionCount;
+
+}
+
+int CountInversion(vector<int>&arr,int start,int end){
+    
+    if (start >= end)return 0;
+    int mid=start+(end-start)/2;
+    int left = CountInversion(arr, start, mid);
+    int right = CountInversion(arr, mid + 1, end);
+    int mergeCount = Merge(arr, start, mid, end);
+
+    return left + right + mergeCount;
+
+    
+}
 
 int main(){
+   
     vector<int>arr={6,3,5,2,7};
     vector<vector<int>>ans=findInversionPair(arr);
     for (const auto& li : ans) {         
@@ -42,6 +75,10 @@ int main(){
     }
     int  ans2=inversionCount(arr);
     cout<<"This is the result of Inverion Count" << ans2<<endl;
+     int start=0;
+    int end=arr.size()-1;
+    int ans3=CountInversion(arr,start,end);
+    cout << ans3<<endl;
 
     return 0;
 }
